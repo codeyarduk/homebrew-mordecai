aa8110ef3b132b630f098bb08c9042edb6a734c3
davidwrossiter <davewrossiter@gmail.com>
2024-11-18 17:37:47 +0000

    fix spacing issue

--- mordecai.rb
+++ mordecai.rb
@@ -2,8 +2,7 @@ class Mordecai < Formula
   desc "CLI tool to link a local codebase to Mordecai"
   homepage "https://github.com/codeyarduk/mordecai"
   url "https://github.com/codeyarduk/mordecai/archive/refs/tags/v0.0.25.tar.gz"
-  sha256 "7708c6bfe86edd6ca7dc4cdb8209b4b6a3724051a68abb1f6
-6e91d5fee0c92f5"
+  sha256 "7708c6bfe86edd6ca7dc4cdb8209b4b6a3724051a68abb1f66e91d5fee0c92f5"
   license "MIT"
 
   depends_on "go" => :build
