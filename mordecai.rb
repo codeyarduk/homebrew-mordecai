f31682f51429df5a3677b919d53402a4bc7cf8a2
davidwrossiter <davewrossiter@gmail.com>
2024-12-03 17:01:09 +0000

    update to version 0.0.30

--- mordecai.rb
+++ mordecai.rb
@@ -1,8 +1,8 @@
 class Mordecai < Formula
   desc "CLI tool to link a local codebase to Mordecai"
   homepage "https://github.com/codeyarduk/mordecai"
-  url "https://github.com/codeyarduk/mordecai/archive/refs/tags/v0.0.29.tar.gz"
-  sha256 "80d037091e99d75a1fc573294663e0d99cd3bcb0d7ad2525473aef95d71dddea"
+  url "https://github.com/codeyarduk/mordecai/archive/refs/tags/v0.0.30.tar.gz"
+  sha256 "5dce1e1b8a7055e6c8921471541b2f61c1d6506b766c9753d27ff7572d953f1d"
   license "MIT"
 
   depends_on "go" => :build
